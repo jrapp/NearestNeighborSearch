d7100993c88c52c525fc9f439e9090a053071bef
jrapp <jeremy.rapp@colorado.edu>
2016-04-22 17:19:29 -0600

    Added the outer most method to deal with the kd tree creation

--- NearestNeighborSearch/src/main/java/com/FastAlgorithms/KDTree.java
+++ NearestNeighborSearch/src/main/java/com/FastAlgorithms/KDTree.java
@@ -1,6 +1,8 @@
 package com.FastAlgorithms;
 
 import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.indexing.INDArrayIndex;
+import org.nd4j.linalg.indexing.NDArrayIndex;
 
 /**
  * Created by Jeremy on 4/22/2016.
@@ -16,6 +18,16 @@ public class KDTree {
         input = myInput;
     }
 
-    
+    public NDArrayIndex[] makeKDTree(){
+        NDArrayIndex[] candidate = new NDArrayIndex[input.shape()[0]]; //Array of indexes for each point in the input
+        NDArrayIndex init = new NDArrayIndex();
+        init.init(0,input.shape()[0]); //Create the initial partition of all of the indices
+        NDArrayIndex[] partitions = createPartitions(init);
+        return candidate;
+    }
+
+    private NDArrayIndex[] createPartitions(INDArrayIndex indices){
+        
+    }
 
 }
